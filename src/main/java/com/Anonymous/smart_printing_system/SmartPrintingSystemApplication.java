ce0755fcafcceb6c0e51ebaa815c31239bda44e6
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-10-26 18:12:45 +0700

    dung-creating a base project for smart printing service

---
+++ src/main/java/com/Anonymous/smart_printing_system/SmartPrintingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.Anonymous.smart_printing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPrintingSystemApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPrintingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Anonymous/smart_printing_system/SmartPrintingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anonymous.smart_printing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPrintingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
